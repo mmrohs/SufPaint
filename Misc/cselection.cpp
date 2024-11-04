ce6c68394e7dcfcab80637662829cde978291444
mmrohs <mmrohs@protonmail.com>
2024-11-04 16:22:59 +0100

    selection class added

--- Misc/cselection.cpp
+++ Misc/cselection.cpp
@@ -1,3 +1,5 @@
 #include "cselection.h"
 
-CSelection::CSelection() {}
+CSelection::CSelection()
+{
+}
--- Misc/cselection.h
+++ Misc/cselection.h
@@ -4,7 +4,7 @@
 class CSelection
 {
 public:
-    CSelection();
+    explicit CSelection();
 };
 
 #endif // CSELECTION_H
