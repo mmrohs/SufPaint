52352ef1497a4a2995a932a133d9addbce4e9030
mmrohs <mmrohs@protonmail.com>
2024-11-12 17:10:39 +0100

    new class CEllipseSelectionTool

---
+++ Tools/cellipseselectiontool.cpp
@@ -0,0 +1,64 @@
+#include "crectangleselectiontool.h"
+#include "../Management/cselectionmanager.h"
+#include "../Widgets/cimageview.h"
+
+
+CRectangleSelectionTool::CRectangleSelectionTool()
+    : CTool(EnumTools::ToolRectSelect)
+{
+}
+
+/*virtual*/ QString CRectangleSelectionTool::GetToolName()
+{
+    return "Rectangle Select";
+}
+
+/*virtual*/ QString CRectangleSelectionTool::GetTooltip()
+{
+    return "Select a rectangular area";
+}
+
+/*virtual*/ QIcon CRectangleSelectionTool::GetToolIcon()
+{
+    QIcon icon("Icons/RectSelection.png");
+    icon.addFile("Icons/RectSelection_Disabled.png", QSize(), QIcon::Mode::Disabled);
+    return icon;
+}
+
+/*virtual*/ void CRectangleSelectionTool::ProcessMousePressEvent(QMouseEvent* pEvent, CImageView* pView)
+{
+    QImage* pImage = GetImage();
+    if (pImage != NULL)
+    {
+        QPoint widgetPos (pEvent->position().x(), pEvent->position().y());
+        QPoint imagePos = pView->GetTrafo()->TransformWidgetToImage(widgetPos);
+        if (pImage->rect().contains(imagePos))
+        {
+            CRectangleSelection* pSelection = new CRectangleSelection();
+            pSelection->AddCoordinate(imagePos);
+            CSelectionManager::GetSelectionManager()->SetSelection(pSelection);
+        }
+    }
+}
+
+/*virtual*/ void CRectangleSelectionTool::ProcessMouseReleaseEvent(QMouseEvent* pEvent, CImageView* pView)
+{
+    CSelectionManager::GetSelectionManager()->ClearSelection();
+}
+
+/*virtual*/ void CRectangleSelectionTool::ProcessMouseMoveEvent(QMouseEvent* pEvent, CImageView* pView)
+{
+    QImage* pImage = GetImage();
+    if (pImage != NULL)
+    {
+        QPoint widgetPos (pEvent->position().x(), pEvent->position().y());
+        QPoint imagePos = pView->GetTrafo()->TransformWidgetToImage(widgetPos);
+        if (pImage->rect().contains(imagePos))
+        {
+            CSelection* pSelection = CSelectionManager::GetSelectionManager()->GetSelection();
+            if (pSelection != NULL)
+                pSelection->AddCoordinate(imagePos);
+            CSelectionManager::GetSelectionManager()->SetSelection(pSelection);
+        }
+    }
+}
---
+++ Tools/cellipseselectiontool.h
@@ -0,0 +1,21 @@
+#ifndef CRECTANGLESELECTIONTOOL_H
+#define CRECTANGLESELECTIONTOOL_H
+
+#include "ctool.h"
+
+
+class CRectangleSelectionTool : public CTool
+{
+public:
+    explicit CRectangleSelectionTool();
+
+    virtual QString GetToolName() override;
+    virtual QString GetTooltip() override;
+    virtual QIcon   GetToolIcon() override;
+
+    virtual void ProcessMousePressEvent(QMouseEvent* pEvent, CImageView* pView) override;
+    virtual void ProcessMouseReleaseEvent(QMouseEvent* pEvent, CImageView* pView) override;
+    virtual void ProcessMouseMoveEvent(QMouseEvent* pEvent, CImageView* pView) override;
+};
+
+#endif // CRECTANGLESELECTIONTOOL_H
